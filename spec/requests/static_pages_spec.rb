f5a59c8a758b23040cf7c622bf7c0049a14f1115
danbender <mail@danbender.net>
2013-12-16 11:16:39 -0800

    * wrote tests for title of each page * refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,39 @@
 require 'spec_helper'
 
 describe 'StaticPages' do
-
+  # possible refactor?
+  # before(:each) { visit '/static_pages' }
   describe 'Home' do
+  # before(:each) { visit '/home' }
+    before(:each) { visit '/static_pages/home' }
+
     it "should have the content 'Welcome'" do
-      visit '/static_pages/home'
       expect(page).to have_content('Welcome')
+    end
+    it "should have a title" do
+      expect(page).to have_title('TDD Twitter | Home')
     end
   end
 
   describe 'Help' do
+    before(:each) { visit '/static_pages/help' }
+
     it "should have the content 'Find Help here'" do
-      visit '/static_pages/help'
       expect(page).to have_content('Find Help here')
+    end
+    it "should have a title" do
+      expect(page).to have_title('TDD Twitter | Help')
     end
   end
 
   describe 'About' do
+    before(:each) { visit '/static_pages/about' }
+
     it "should have the content 'About'" do
-      visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+    it "should have a title" do
+      expect(page).to have_title('TDD Twitter | About')
     end
   end
 
