4775fce3808a77502e3a5a39b727ba805ca8649b
danbender <mail@danbender.net>
2013-12-16 10:48:51 -0800

    add test for existence of About in about.html.erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,24 @@ require 'spec_helper'
 
 describe 'StaticPages' do
 
-  describe 'home' do
+  describe 'Home' do
     it "should have the content 'Welcome'" do
       visit '/static_pages/home'
       expect(page).to have_content('Welcome')
     end
   end
 
-  describe 'help' do
+  describe 'Help' do
     it "should have the content 'Find Help here'" do
       visit '/static_pages/help'
       expect(page).to have_content('Find Help here')
+    end
+  end
+
+  describe 'About' do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
     end
   end
 
