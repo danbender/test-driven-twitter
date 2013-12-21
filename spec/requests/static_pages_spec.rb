a454a3332a771efaba9d88a310e72ab897534ad5
danbender <mail@danbender.net>
2013-12-21 12:36:26 +0100

    remove let statement, add reference to full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,30 +4,28 @@ describe 'StaticPages' do
 
   subject { page }
 
-  let(:base_title) { 'TDD Twitter'}
-
   describe 'Home' do
     before(:each) { visit root_path }
     it { should have_content('Welcome') }
-    it { should have_title("#{base_title}") }
+    it { should have_title(full_title('')) }
     it { should_not have_title(" | Home") }
   end
 
   describe 'Help' do
     before(:each) { visit help_path }
     it { should have_content('Find Help here') }
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
   end
 
   describe 'About' do
     before(:each) { visit about_path }
     it { should have_content('About') }
-    it { should have_title("#{base_title} | About") }
+    it { should have_title(full_title('About')) }
   end
 
   describe 'Contact' do
     before(:each) { visit contact_path  }
     it { should have_content('Contact us') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
