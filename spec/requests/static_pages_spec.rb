8926331ff64a1e1edc77c5dabf58d9e441b2e73f
danbender <mail@danbender.net>
2013-12-21 14:29:14 +0100

    * refactor tests to use shared examples and let * add tests for working links in nav (header and footer) * write tests for full_title helper in application_helper_spec * replace full_title helper with ApplicationHelper * all tests pass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,28 +3,55 @@ require 'spec_helper'
 describe 'StaticPages' do
   subject { page }
 
+  shared_examples_for 'all static pages' do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(page_title)}
+  end
+
   describe 'Home' do
-    before(:each) { visit root_path }
-    it { should have_content('Welcome') }
-    it { should have_title(full_title('')) }
+    before(:each)     { visit root_path }
+    let(:heading)     { 'Welcome' }
+    let(:page_title)  { '' }
+
+    it_should_behave_like 'all static pages'
     it { should_not have_title(" | Home") }
   end
 
   describe 'Help' do
-    before(:each) { visit help_path }
-    it { should have_content('Find Help here') }
-    it { should have_title(full_title('Help')) }
+    before(:each)     { visit help_path }
+    let(:heading)     { 'Find Help here' }
+    let(:page_title)  { 'Help' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe 'About' do
-    before(:each) { visit about_path }
-    it { should have_selector('h1', text: 'About') }
-    it { should have_title(full_title('About')) }
+    before(:each)     { visit about_path }
+    let(:heading)     { 'About' }
+    let(:page_title)  { 'About' }
+
+    it_should_behave_like 'all static pages'
   end
 
   describe 'Contact' do
-    before(:each) { visit contact_path  }
-    it { should have_selector('h1', text: 'Contact us') }
-    it { should have_title(full_title('Contact')) }
+    before(:each)     { visit contact_path  }
+    let(:heading)     { 'Contact us' }
+    let(:page_title)  { 'Contact' }
+
+    it_should_behave_like 'all static pages'
+  end
+
+  it "should test the links of nav in header and footer" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "Sign up"
+    expect(page).to have_title(full_title('Sign up'))
   end
 end
