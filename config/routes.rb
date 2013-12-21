89afdde5b009174379eacb6ae7d2b648fe6b3f51
danbender <mail@danbender.net>
2013-12-21 12:49:50 +0100

    * add users controller * add users#new and match to /signup * add new.html.erb * all tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ TddTwitter::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+
+  resources :users, only: [:new]
+  match '/signup', to: 'users#new', via: 'get'
 end
