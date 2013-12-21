2b07e88ea7d50e7d059e83e3914a4bc074c44364
danbender <mail@danbender.net>
2013-12-21 09:55:38 +0100

    * remove dupl for root_path in config/routes * refactor tests to find new shortened routes * all tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 TddTwitter::Application.routes.draw do
-
   root :to => "static_pages#home"
-  match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
