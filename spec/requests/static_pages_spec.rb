382c1e6e15e168ac2bd2b8279ab1d23c267eef2b
danbender <mail@danbender.net>
2013-12-21 12:09:29 +0100

    refactor tests, remove duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,33 +1,33 @@
 require 'spec_helper'
 
 describe 'StaticPages' do
+
   subject { page }
 
   let(:base_title) { 'TDD Twitter'}
 
   describe 'Home' do
-    before(:each) { visit '/' }
+    before(:each) { visit root_path }
     it { should have_content('Welcome') }
     it { should have_title("#{base_title}") }
     it { should_not have_title(" | Home") }
   end
 
   describe 'Help' do
-    before(:each) { visit '/help' }
+    before(:each) { visit help_path }
     it { should have_content('Find Help here') }
     it { should have_title("#{base_title} | Help") }
   end
 
   describe 'About' do
-    before(:each) { visit '/about' }
+    before(:each) { visit about_path }
     it { should have_content('About') }
     it { should have_title("#{base_title} | About") }
   end
 
   describe 'Contact' do
-    before(:each) { visit '/contact' }
+    before(:each) { visit contact_path  }
     it { should have_content('Contact us') }
     it { should have_title("#{base_title} | Contact") }
   end
-
 end
