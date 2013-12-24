c1715ac2234a1d55d1f0fa40953728f4f8fdd566
danbender <mail@danbender.net>
2013-12-24 15:39:30 +0100

    * add tests for email uniqueness * implemente email uniqueness in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,22 +10,22 @@ describe User do
   # sanity check
   it { should be_valid }
 
-  describe 'when name is not present' do
+  describe 'name is not present' do
     before { @user.name = '' }
     it { should_not be_valid }
   end
 
-  describe 'when email is not present' do
+  describe 'email is not present' do
     before { @user.email = '' }
     it { should_not be_valid }
   end
 
-  describe 'when name is too long' do
+  describe 'name is too long' do
     before { @user.name = 'a' * 31 }
     it { should_not be_valid }
   end
 
-  describe 'when email format is invalid' do
+  describe 'email format is invalid' do
     it 'should be invalid' do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
@@ -35,7 +35,7 @@ describe User do
     end
   end
 
-  describe 'when email format is valid' do
+  describe 'email format is valid' do
     it 'should be valid' do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
@@ -43,5 +43,14 @@ describe User do
         expect(@user).to be_valid
       end
     end
+  end
+
+  describe 'email is already taken' do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    it { should_not be_valid }
   end
 end
