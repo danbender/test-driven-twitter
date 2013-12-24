1ca578514d365942ff2a8c5c36dafff3388d49bb
danbender <mail@danbender.net>
2013-12-24 01:48:58 +0100

    * add tests for email format validation * implement email format check in User model (regex) * all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :name, :email
 
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
