a81c27247f698c222ac4cad646b7c162b5452755
danbender <mail@danbender.net>
2013-12-24 01:34:32 +0100

    * write tests for validations * add validations * all tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :name, :email
+
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { @user = User.new(name: 'test user', email: 'mail@example.com')}
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  # sanity check
+  it { should be_valid }
+
+  describe 'when name is not present' do
+    before { @user.name = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when email is not present' do
+    before { @user.email = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when name is too long' do
+    before { @user.name = 'a' * 31 }
+    it { should_not be_valid }
+  end
 end
