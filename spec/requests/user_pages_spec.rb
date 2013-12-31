19dcfccb8a2521d31c6158518eb274d363d7d380
danbender <mail@danbender.net>
2013-12-31 17:53:25 +0100

    refactor user pages sign up tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,7 +33,7 @@ describe 'UserPages' do
         fill_in 'Name',                   with: 'Example user'
         fill_in 'Email',                  with: 'user@example.com'
         fill_in 'Password',               with: 'test12345'
-        fill_in 'Password Confirmation',  with: 'test12345'
+        fill_in 'Password confirmation',  with: 'test12345'
       end
       it 'should create a user' do
         expect { click_button submit}.to change(User, :count).by(1)
