fc6af9a8e1fa2ae79da93d693459faf955e65341
danbender <mail@danbender.net>
2014-01-02 22:47:17 +0100

    add tests for sign in with invalid info

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,15 @@ describe 'Authentication' do
     it { should have_content "Sign in" }
     it { should have_title "Sign in" }
   end
+
+  describe 'sign in' do
+    before { visit signin_path }
+
+    describe 'with invalid info' do
+      before { click_button 'Sign in' }
+
+      it { should have_title 'Sign in' }
+      it { should have_selector 'div.alert.alert-error' }
+    end
+  end
 end
