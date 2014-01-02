fd1ead2eac1eb6397e3a4536ca059253ae2d4852
danbender <mail@danbender.net>
2014-01-02 22:22:15 +0100

    fix routing error: /signin must be sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ TddTwitter::Application.routes.draw do
 
   resources :sessions, only: [ :new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#create',      via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :users
