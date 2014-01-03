4c7d21691de71f12f1fbe1a8baea3cb6538e5339
danbender <mail@danbender.net>
2014-01-03 16:36:47 +0100

    begin to handle successfull signin with (yet undefined) method sign_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #create session
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid Email or Password'
       render 'new'
