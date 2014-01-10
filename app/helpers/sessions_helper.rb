465d177dcd63d877785605c679e57566f6000648
danbender <mail@danbender.net>
2014-01-10 15:37:39 +0100

    add helper to store requested url and redirect back to it after sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,15 @@ module SessionsHelper
     user == current_user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
   def sign_out
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
