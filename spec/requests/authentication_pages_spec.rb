df6a597371348a1fdcd78f2f76abb757201b7d6e
danbender <mail@danbender.net>
2014-01-10 14:50:09 +0100

    remove comments and commented out code from spec file

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,7 +36,6 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
 
-      # it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
@@ -54,36 +53,6 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-      # describe "when attempting to visit a protected page" do
-      #   before do
-      #     visit edit_user_path(user)
-      #     fill_in "Email",    with: user.email
-      #     fill_in "Password", with: user.password
-      #     click_button "Sign in"
-      #   end
-
-      #   describe "after signing in" do
-
-      #     it "should render the desired protected page" do
-      #       page.should have_title('Edit user')
-      #     end
-
-      #     describe "when signing in again" do
-      #       before do
-      #         delete signout_path
-      #         visit signin_path
-      #         fill_in "Email",    with: user.email
-      #         fill_in "Password", with: user.password
-      #         click_button "Sign in"
-      #       end
-
-      #       it "should render the default (profile) page" do
-      #         page.should have_title(user.name)
-      #       end
-      #     end
-      #   end
-      # end
-
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -96,74 +65,7 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-        # describe "visiting user index" do
-        #   before { visit users_path }
-        #   it { should have_title('Sign in') }
-        # end
-
-        # describe "visiting the following page" do
-        #   before { visit following_user_path(user) }
-        #   it { should have_title('Sign in') }
-        # end
-
-        # describe "visiting the followers page" do
-        #   before { visit followers_user_path(user) }
-        #   it { should have_title('Sign in') }
-        # end
       end
-
-      # describe "in the Microposts controller" do
-
-      #   describe "submitting to the create action" do
-      #     before { post microposts_path }
-      #     specify { response.should redirect_to(signin_url) }
-      #   end
-
-      #   describe "submitting to the destroy action" do
-      #     before { delete micropost_path(FactoryGirl.create(:micropost)) }
-      #     specify { response.should redirect_to(signin_url) }
-      #   end
-      # end
-
-      # describe "in the Relationships controller" do
-      #   describe "submitting to the create action" do
-      #     before { post relationships_path }
-      #     specify { response.should redirect_to(signin_url) }
-      #   end
-
-      #   describe "submitting to the destroy action" do
-      #     before { delete relationship_path(1) }
-      #     specify { response.should redirect_to(signin_url) }
-      #   end
-      # end
-    # end
-
-    # describe "as wrong user" do
-    #   let(:user) { FactoryGirl.create(:user) }
-    #   let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-    #   before { sign_in user, no_capybara: true }
-
-    #   describe "visiting Users#edit page" do
-    #     before { visit edit_user_path(wrong_user) }
-    #     it { should have_title(full_title('')) }
-    #   end
-
-    #   describe "submitting a PATCH request to the Users#update action" do
-    #     before { patch user_path(wrong_user) }
-    #     specify { response.should redirect_to(root_url) }
-    #   end
-    # end
-
-    # describe "as non-admin user" do
-    #   let(:user) { FactoryGirl.create(:user) }
-    #   let(:non_admin) { FactoryGirl.create(:user) }
-
-    #   before { sign_in non_admin, no_capybara: true }
-
-    #   describe "submitting a DELETE request to the Users#destroy action" do
-    #     before { delete user_path(user) }
-    #     specify { response.should redirect_to(root_url) }
-    #   end
     end
   end
 end
