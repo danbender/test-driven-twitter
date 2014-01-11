92add92eaac4b2220d08baabc3bc4c2f1ee95099
danbender <mail@danbender.net>
2014-01-12 00:59:10 +0100

    refactor test

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-   sequence(:name) { |n| "Person #{n}" }
-   sequence(:email) { |n| "person_#{n}@test.com" }
+   sequence(:name) { |n| "Person-#{n}" }
+   sequence(:email) { |n| "person-#{n}@test.com" }
    password "foobar"
    password_confirmation "foobar"
   end
