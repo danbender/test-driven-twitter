6de558ae1e3c8ad12c551173994f19df1368accb
danbender <mail@danbender.net>
2014-01-14 16:12:12 +0100

    tests for validity of micropost

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
   before do
+    # @micropost = user.microposts.build(content: "Lorem ipsum")
     @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
   end
 
@@ -10,4 +11,11 @@ describe Micropost do
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
