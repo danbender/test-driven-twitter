a3f63f0efe1282fff98d0703388a101038ef9e20
danbender <mail@danbender.net>
2014-01-18 22:58:32 +0100

    fix; all tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
-  it { should respond_to(:micropostsg) }
+  it { should respond_to(:microposts) }
   # sanity check
   it { should be_valid }
   it { should_not be_admin }
