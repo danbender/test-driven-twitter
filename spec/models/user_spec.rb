d4ce5a27f0a5fd46b72eb2328002146f227fa38e
danbender <mail@danbender.net>
2014-01-19 19:11:04 +0100

    testing the order of a user's tweets

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -123,4 +123,18 @@ describe User do
     # equivalent to
     # it { expect(@user.remember_token).not_to be_blank }
   end
+
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
 end
