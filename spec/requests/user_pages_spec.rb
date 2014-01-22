d424f3372b92a434810b4419cfb3cd30c563ea0d
danbender <mail@danbender.net>
2014-01-22 19:24:30 +0100

    test for displaying tweets on a user's show page'

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,6 +5,7 @@ describe "UserPages" do
 
   describe "index" do
     let(:user) { FactoryGirl.create(:user) }
+
     before(:each) do
       sign_in user
       visit users_path
@@ -57,10 +58,19 @@ describe "UserPages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_title(full_title(user.name)) }
     it { should have_content(user.name) }
+
+    describe "tweets" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.countg) }
+    end
   end
 
   describe "signup" do
