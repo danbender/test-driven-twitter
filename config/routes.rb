7ffce95fd77ebd14495fdddc805df9a019ee1f80
danbender <mail@danbender.net>
2014-03-27 23:46:54 +0100

    reaarange routes for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 TddTwitter::Application.routes.draw do
+  resources :sessions, only: [ :new, :create, :destroy]
+  resources :users
+
   root :to => "static_pages#home"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
 
-  resources :sessions, only: [ :new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-
-  resources :users
 end
