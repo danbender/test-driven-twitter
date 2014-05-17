b2d33057121dc327fb5c4fa8eb01655bdbb01e73
danbender <mail@danbender.net>
2014-05-18 01:21:15 +0200

    test for unfollowing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,7 @@ describe User do
   it { should respond_to(:followed_users) }
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
   it { should be_valid } # sanity check
   it { should_not be_admin } # sanity check
 
