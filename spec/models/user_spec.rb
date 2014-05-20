9ef3ec3f30ca4d98a114d5648fe78dbf16ada13f
danbender <mail@danbender.net>
2014-05-20 21:40:30 +0200

    spec / all tests pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -164,20 +164,25 @@ describe User do
     end
   end
 
-  describe "following" do
+ describe "following" do
     let(:other_user) { FactoryGirl.create(:user) }
     before do
       @user.save
-      @user.follow(:other_user)
+      @user.follow!(other_user)
     end
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
 
-    describe "unfollowing" do
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
       before { @user.unfollow!(other_user) }
 
-      it { should_not be_following(other_user)}
+      it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
   end
