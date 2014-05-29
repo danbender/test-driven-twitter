9d787703bdecddc28565b64bb7f3a7355519d8cc
danbender <mail@danbender.net>
2014-05-29 22:21:46 +0200

    test for authorization of user when visiting follower/following pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -107,6 +107,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title("Sign in") }
         end
+
+        describe 'visiting the following page' do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe 'visiting the folloers page' do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "in the Tweets controller" do
